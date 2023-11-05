0248f591946d76de44691bdcee0851bd8fef8f74
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-05 23:33:33 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useRecoilValue } from "recoil";
 import "./App.css";
 import TodoItemCreator from "./components/TodoItemCreator";
+import { todoListState } from "./todoAtoms";
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
+  console.log(todoList);
   return (
     <div className="App">
       <TodoItemCreator />
