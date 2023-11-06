0c9ed277e0fdd9d946fb98c8b20389e2b9c8d494
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-06 23:15:13 +0900

    updated

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../todoAtoms";
 
 const TodoItem = ({ item }) => {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
   return (
     <div>
       <input type="text" value={item.text} />
