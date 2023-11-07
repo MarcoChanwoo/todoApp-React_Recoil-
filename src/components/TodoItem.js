f8f4ce1a231c1260399a146c833df343be40cbfb
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-07 23:32:33 +0900

    updated

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -17,6 +17,7 @@ const TodoItem = ({ item }) => {
   return (
     <div>
       <input type="text" value={item.text} onChange={editItemText} />
+      <input type="checkbox" checked={true} />
     </div>
   );
 };
