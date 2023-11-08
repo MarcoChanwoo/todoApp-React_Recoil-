fc24bd3cba795b905ce5f5e21cc31176e796dc7c
MarcoChanwoo <cwhong8087@gmail.com>
2023-11-08 23:28:12 +0900

    updated; wq -wq git

--- src/todoAtoms.js
+++ src/todoAtoms.js
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const todoListState = atom({
   key: "todoListState",
@@ -8,4 +8,12 @@ export const todoListState = atom({
 export const todoListFilterState = atom({
   key: "todoListFilterState",
   default: "Show All",
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+  },
 });
